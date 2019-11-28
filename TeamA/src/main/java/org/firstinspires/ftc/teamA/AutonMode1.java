e99d10480a0df606d06d8331e55962f6db6a4116
Hervé <herve.schmitveiler@icloud.com>
2019-11-28 10:35:39 +0800

    SkystoneAuton Parent Opmode and implementation of it as child

---
+++ TeamA/src/main/java/org/firstinspires/ftc/teamA/AutonMode1.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamA;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import helperClassesA.DriveStateMachine;
+import helperClassesA.SkystoneAutonMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+@Autonomous
+public class AutonMode1 extends SkystoneAutonMode {
+
+
+    @Override
+    public void runOpMode(){
+
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+
+        DriveStateMachine robotState = new DriveStateMachine(leftFront, rightFront, leftBack, rightBack, this, DriveStateMachine.DriveTrain.DRIVE_TRAIN_MECANUM);
+
+        waitForStart();
+        initVuforia();
+        targetsSkyStone.activate();
+
+        /**
+         *
+         * DO STUFF
+         *
+         */
+
+    }
+
+
+    public AutonMode1()
+    {
+        CAMERA_FORWARD_DISPLACEMENT = 0.0f;
+        CAMERA_LEFT_DISPLACEMENT = 0.0f;
+        CAMERA_VERTICAL_DISPLACEMENT = 0.0f;
+    }
+
+}
