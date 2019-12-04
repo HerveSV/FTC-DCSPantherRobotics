de2c377f6d20f67467dd502ba7aca524f82f08cb
dan <unorderedpizza@gmail.com>
2019-12-04 13:24:34 +0800

    add offset feature

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/keepMotorInPos.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/keepMotorInPos.java
@@ -10,24 +10,53 @@ import helperClasses.PIDFmanager;
 @TeleOp
 public class keepMotorInPos extends LinearOpMode {
     private DcMotor motor;
-    private static final float MAXPOWER = 1;
-    private static final float THRESHOLD = 500;
+    private static final float MAXPOWER = 1.0f;
+    private static final float THRESHOLD = 500.0f;
+    private static final int STEP = 1;
+    private static final int OFFSET_BOUND = 500;
 
     @Override
     public void runOpMode() {
         waitForStart();
+
+        int offset = 0;
+
         motor = hardwareMap.get(DcMotor.class, "testMotor");
         PIDFmanager.setPIDF((DcMotorEx) motor);
         motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         while(opModeIsActive())
         {
             telemetry.addData("Encoder counts:", motor.getCurrentPosition());
-            motor.setPower(1.0f * getPower(0, motor.getCurrentPosition()));
+            motor.setPower(1.0f * getPower(0, motor.getCurrentPosition(), offset));
         }
     }
-    public float getPower(int target, int current) {
+    private float getPower(int target, int current, int offset) {
         int diff = target - current;
+
+        if (diff > 0) {
+            if (offset < 0) {
+                offset += STEP;
+            } else {
+                offset -= STEP;
+            }
+        }
+        if (diff < 0) {
+            if (offset > 0) {
+                offset -= STEP;
+            } else {
+                offset += STEP;
+            }
+        }
+        if (offset > OFFSET_BOUND) {
+            offset = OFFSET_BOUND;
+        } else if (offset < - OFFSET_BOUND) {
+            offset = - OFFSET_BOUND;
+        }
+
+        diff = target + offset - current;
+
         float val = MAXPOWER / THRESHOLD * diff;
         if (Math.abs(diff) < THRESHOLD) {
             return val;
